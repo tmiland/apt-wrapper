195835053ca6d95aa7c1d74f386efcd3d5ebd9e5
Tommy Miland <kontakt@tmiland.com>
2022-11-19 23:48:22 +0100

    Add "add-apt-repository"
    - Added "add-apt-repository"  to easily add repositories from launchpad for Debian systems
    - Fetch the only corresponding apt package list immediately after added ppa repo

--- apt-wrapper.sh
+++ apt-wrapper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+# shellcheck disable=SC2086,SC2181
 ## Author: Tommy Miland (@tmiland) - Copyright (c) 2022
 
 VERSION='1.0.0'
@@ -82,25 +82,26 @@ help() {
   echo
   echo "  If called without arguments, shows help."
   echo
-  printf "%s\\n" "  ${YELLOW}help         ${NORMAL}|h      ${GREEN}display this help and exit${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}update       ${NORMAL}|up     ${GREEN}update package information${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}upgrade      ${NORMAL}|upg    ${GREEN}upgrade available packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}full-upgrade ${NORMAL}|fupg   ${GREEN}full-upgrade. See: man apt(8)${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}install      ${NORMAL}|i      ${GREEN}install one or more packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}deb-install  ${NORMAL}|di     ${GREEN}install local deb package${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}reinstall    ${NORMAL}|ri     ${GREEN}reinstall one or more packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}remove       ${NORMAL}|r      ${GREEN}remove one or more packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}purge        ${NORMAL}|p      ${GREEN}purge one or more packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}autoremove   ${NORMAL}|ar     ${GREEN}clean up unused dependencies${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}autoclean    ${NORMAL}|ac     ${GREEN}clears out the local repository${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}clean        ${NORMAL}|c      ${GREEN}clears out the local repository${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}show         ${NORMAL}|sh     ${GREEN}Show information about package(s)${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}policy       ${NORMAL}|p      ${GREEN}displays information about the package(s)${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}list         ${NORMAL}|l      ${GREEN}display a list of packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}edit-sources ${NORMAL}|es     ${GREEN}lets you edit your sources.list${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}search       ${NORMAL}|s      ${GREEN}search for available packages${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}find         ${NORMAL}|f      ${GREEN}package searching utility${NORMAL}"
-  printf "%s\\n" "  ${YELLOW}apt-mark     ${NORMAL}|am     ${GREEN}set/unset settings for a package${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}help               ${NORMAL}|h      ${GREEN}display this help and exit${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}update             ${NORMAL}|up     ${GREEN}update package information${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}upgrade            ${NORMAL}|upg    ${GREEN}upgrade available packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}full-upgrade       ${NORMAL}|fupg   ${GREEN}full-upgrade. See: man apt(8)${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}install            ${NORMAL}|i      ${GREEN}install one or more packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}deb-install        ${NORMAL}|di     ${GREEN}install local deb package${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}reinstall          ${NORMAL}|ri     ${GREEN}reinstall one or more packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}remove             ${NORMAL}|r      ${GREEN}remove one or more packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}purge              ${NORMAL}|p      ${GREEN}purge one or more packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}autoremove         ${NORMAL}|ar     ${GREEN}clean up unused dependencies${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}autoclean          ${NORMAL}|ac     ${GREEN}clears out the local repository${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}clean              ${NORMAL}|c      ${GREEN}clears out the local repository${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}show               ${NORMAL}|sh     ${GREEN}Show information about package(s)${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}policy             ${NORMAL}|p      ${GREEN}displays information about the package(s)${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}list               ${NORMAL}|l      ${GREEN}display a list of packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}edit-sources       ${NORMAL}|es     ${GREEN}lets you edit your sources.list${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}search             ${NORMAL}|s      ${GREEN}search for available packages${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}find               ${NORMAL}|f      ${GREEN}package searching utility${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}apt-mark           ${NORMAL}|am     ${GREEN}set/unset settings for a package${NORMAL}"
+  printf "%s\\n" "  ${YELLOW}add-apt-repository ${NORMAL}|aar    ${GREEN}add apt repo from ppa.launchpad.net${NORMAL}"
   echo
   printf "%s\\n" "  Script version: ${CYAN}${VERSION}${NORMAL} | Enable apt progressbar with --progress-bar"
   echo
@@ -121,6 +122,56 @@ fi
 apt="apt"
 dpkg="dpkg"
 sudo="sudo"
+
+add-apt-repository() {
+  # Credits: 
+  # Fetch the only corresponding apt package list immediately after added ppa repo. - https://github.com/PeterDaveHello/add-apt-ppa
+  # Easily add repositories from launchpad for Debian systems. - https://github.com/timothyvanderaerden/add-apt-repository
+  LAUNCHPAD=http://ppa.launchpad.net/
+  INPUT=$1
+  if [ -z ${2+x} ]; then
+    # Use jammy as default
+    VERSION=jammy
+  else
+    # Else use provided second argument
+    VERSION=$2
+  fi
+  # get ppa: git-core/ppa
+  PPA=$(echo "$INPUT" | cut -d ':' -f 2)
+  # Cuts down to: git-core
+  SOFTWARE=$(echo "$PPA" | cut -d '/' -f 1)
+  URL="$LAUNCHPAD$PPA/ubuntu $VERSION main"
+  TEMP_DIR=$(mktemp -d)
+  TEMP_KEY=$TEMP_DIR/archive_keyring
+  # Create source list file
+  file=/etc/apt/sources.list.d/$SOFTWARE.list
+  archive_keyring="/usr/share/keyrings/${SOFTWARE}-archive-keyring.gpg"
+  ${sudo} touch "$file"
+  echo "deb [signed-by=$archive_keyring] $URL
+# deb-src $URL" | ${sudo} tee "$file" >/dev/null
+  # Add key
+  # Here we run apt update on the added repo only to get the missing key (for faster update)
+  ${sudo} "${apt}" update -o Dir::Etc::sourcelist="$file" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" >> /dev/null 2> $TEMP_KEY
+  KEY=$(cat $TEMP_KEY | cut -d ':' -f 6 | cut -d ' ' -f 3)
+  # Here we're importing the key from keyserver, exporting to file, dearmoring and deleting when done.
+  # Credit: https://github.com/Ulauncher/Ulauncher/issues/972#issuecomment-1034751651
+  ${sudo} gpg --keyserver keyserver.ubuntu.com --recv $KEY &> /dev/null
+  ${sudo} gpg --export --armor $KEY | ${sudo} gpg --dearmor | ${sudo} tee $archive_keyring &> /dev/null
+  # Delete key without confirmation: https://stackoverflow.com/questions/9768473/gnupg-suppress-message-while-deleting-public-key
+  ${sudo} gpg --batch --yes --delete-key $KEY &> /dev/null
+  $sudo chmod 644 $archive_keyring &> /dev/null
+  ${sudo} rm -rf $TEMP_DIR &> /dev/null
+  # Update repo once more
+  ${sudo} "${apt}" update -o Dir::Etc::sourcelist="$file" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0" &> /dev/null
+  #success message
+  if [ -n "$KEY" ]; then
+    echo ""
+    echo "Key added to $archive_keyring"
+    echo ""
+  fi
+  echo "$LAUNCHPAD$PPA added to your system"
+  echo ""
+}
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -206,6 +257,11 @@ while [[ $# -gt 0 ]]; do
     apt-mark|am)
       shift
       ${sudo} "${apt}" apt-mark "$@"
+      break
+      ;;
+    add-apt-repository|aar)
+      shift
+      add-apt-repository "$@"
       break
       ;;
     help|-h|--help|-help)
